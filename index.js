c6b3ffe36aa81a1b654a868775d601cd02757757
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-02-24 16:09:36 -0300

    Index.js added

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var http = require('http'),
+    express = require('express');
+
+var app = express();
+app.set('port', process.env.PORT || 3000); 
+
+app.get('/', function (req, res) {
+	console.log('Invalid request received.');
+	res.json({'error': 'invalid request'});
+});
+
+app.get('/create_pod', function (req, res) {
+	console.log('Request received.');
+	var version = req.query.v;
+	var title = 'Creating pod for version ' + version;
+
+    res.send('<html><body><h>' + title + '</h></body></html>');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
