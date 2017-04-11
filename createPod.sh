84d03b29e2c477872bc32be2ec26fb62c5c71e15
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-04-11 11:59:30 -0300

    Register for pod

--- createPod.sh
+++ createPod.sh
@@ -63,7 +63,7 @@ fi
 echo "=========================================="
 echo "3) Push podspec into trunk/Specs"
 echo "=========================================="
-pod trunk register cristina.rodriguez@mercadolibre.com 'Pod Service' --description='macbook pro'
+pod trunk register crisyelpegaso@gmail.com 'Pod Service' --description='macbook pro'
 pod trunk push $PODSPEC_FILE --allow-warnings --verbose
 POD_TRUNK_STATUS=$?
 
